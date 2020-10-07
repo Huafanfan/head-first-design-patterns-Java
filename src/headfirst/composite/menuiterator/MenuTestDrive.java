87342fb220858a10313a4d5671d05daceb33de03
Huafanfan <fanhua0425@gmail.com>
2020-10-07 16:19:13 +0800

    update

--- src/headfirst/composite/menuiterator/MenuTestDrive.java
+++ src/headfirst/composite/menuiterator/MenuTestDrive.java
@@ -16,7 +16,7 @@ public class MenuTestDrive {
   
 		allMenus.add(pancakeHouseMenu);
 		allMenus.add(dinerMenu);
-		allMenus.add(cafeMenu);
+		//allMenus.add(cafeMenu);
   
 		pancakeHouseMenu.add(new MenuItem(
 			"K&B's Pancake Breakfast", 
@@ -41,69 +41,69 @@ public class MenuTestDrive {
 
 		dinerMenu.add(new MenuItem(
 			"Vegetarian BLT",
-			"(Fakin') Bacon with lettuce & tomato on whole wheat", 
-			true, 
+			"(Fakin') Bacon with lettuce & tomato on whole wheat",
+			true,
 			2.99));
 		dinerMenu.add(new MenuItem(
 			"BLT",
-			"Bacon with lettuce & tomato on whole wheat", 
-			false, 
+			"Bacon with lettuce & tomato on whole wheat",
+			false,
 			2.99));
 		dinerMenu.add(new MenuItem(
 			"Soup of the day",
-			"A bowl of the soup of the day, with a side of potato salad", 
-			false, 
+			"A bowl of the soup of the day, with a side of potato salad",
+			false,
 			3.29));
 		dinerMenu.add(new MenuItem(
 			"Hotdog",
 			"A hot dog, with saurkraut, relish, onions, topped with cheese",
-			false, 
+			false,
 			3.05));
 		dinerMenu.add(new MenuItem(
 			"Steamed Veggies and Brown Rice",
-			"A medly of steamed vegetables over brown rice", 
-			true, 
+			"A medly of steamed vegetables over brown rice",
+			true,
 			3.99));
- 
+
 		dinerMenu.add(new MenuItem(
 			"Pasta",
 			"Spaghetti with Marinara Sauce, and a slice of sourdough bread",
-			true, 
+			true,
 			3.89));
-   
-		dinerMenu.add(dessertMenu);
-  
-		dessertMenu.add(new MenuItem(
-			"Apple Pie",
-			"Apple pie with a flakey crust, topped with vanilla icecream",
-			true,
-			1.59));
-		dessertMenu.add(new MenuItem(
-			"Cheesecake",
-			"Creamy New York cheesecake, with a chocolate graham crust",
-			true,
-			1.99));
-		dessertMenu.add(new MenuItem(
-			"Sorbet",
-			"A scoop of raspberry and a scoop of lime",
-			true,
-			1.89));
 
-		cafeMenu.add(new MenuItem(
-			"Veggie Burger and Air Fries",
-			"Veggie burger on a whole wheat bun, lettuce, tomato, and fries",
-			true, 
-			3.99));
-		cafeMenu.add(new MenuItem(
-			"Soup of the day",
-			"A cup of the soup of the day, with a side salad",
-			false, 
-			3.69));
-		cafeMenu.add(new MenuItem(
-			"Burrito",
-			"A large burrito, with whole pinto beans, salsa, guacamole",
-			true, 
-			4.29));
+		//dinerMenu.add(dessertMenu);
+		//
+		//dessertMenu.add(new MenuItem(
+		//	"Apple Pie",
+		//	"Apple pie with a flakey crust, topped with vanilla icecream",
+		//	true,
+		//	1.59));
+		//dessertMenu.add(new MenuItem(
+		//	"Cheesecake",
+		//	"Creamy New York cheesecake, with a chocolate graham crust",
+		//	true,
+		//	1.99));
+		//dessertMenu.add(new MenuItem(
+		//	"Sorbet",
+		//	"A scoop of raspberry and a scoop of lime",
+		//	true,
+		//	1.89));
+		//
+		//cafeMenu.add(new MenuItem(
+		//	"Veggie Burger and Air Fries",
+		//	"Veggie burger on a whole wheat bun, lettuce, tomato, and fries",
+		//	true,
+		//	3.99));
+		//cafeMenu.add(new MenuItem(
+		//	"Soup of the day",
+		//	"A cup of the soup of the day, with a side salad",
+		//	false,
+		//	3.69));
+		//cafeMenu.add(new MenuItem(
+		//	"Burrito",
+		//	"A large burrito, with whole pinto beans, salsa, guacamole",
+		//	true,
+		//	4.29));
  
 		Waitress waitress = new Waitress(allMenus);
    
